c18ae71a01ed8ebba97060c953843d38ed868ca2
Jerry Hsu <Jerry>
2021-10-15 18:56:31 -0400

    Tree Exercises

--- tree.js
+++ tree.js
@@ -15,20 +15,47 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    const toVisitQueue = [this.root];
+    let sum = 0;
+    while (this.root != null && toVisitQueue.length) {
+      const current = toVisitQueue.shift();
+      sum += current.val;
+      for (let child of current.children) {
+        toVisitQueue.push(child);
+      }
+    }
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
-
+    const toVisitQueue = [this.root];
+    let count = 0;
+    while (this.root != null && toVisitQueue.length) {
+      const current = toVisitQueue.shift();
+      if (current.val % 2 === 0) count++;
+      for (let child of current.children) {
+        toVisitQueue.push(child);
+      }
+    }
+    return count;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
-
+    const toVisitQueue = [this.root];
+    let count = 0;
+    while (this.root != null && toVisitQueue.length) {
+      const current = toVisitQueue.shift();
+      if (current.val > lowerBound) count++;
+      for (let child of current.children) {
+        toVisitQueue.push(child);
+      }
+    }
+    return count;
   }
 }
 
